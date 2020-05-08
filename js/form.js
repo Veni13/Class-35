172fbbb735e225df0c9f9a21d3d5331024be936d
Veni13 <veni7678@gmail.com>
2020-05-08 18:59:01 +0530

    made the player login and play the game

---
+++ js/form.js
@@ -0,0 +1,28 @@
+class Form{
+    constructor(){
+
+    }
+    display(){
+        var title = createElement('h2');
+        title.html("Car Racing Game");
+        title.position(130,0);
+        var input = createInput("name");
+        var button = createButton('Play');
+        var greeting = createElement('h3');
+        input.position(130,60);
+        button.position(250,100);
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+            var name = input.value();
+            playerCount+= 1;
+            player.update(name)
+            player.updateCount(playerCount);
+            greeting.html("Hello!"+ name)
+            greeting.position(130,160);
+            
+        })
+
+        
+    }
+}
